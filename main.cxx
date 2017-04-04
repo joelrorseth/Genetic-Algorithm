c5298c9180293bc50446d0fa3e1681bacd60e03f
Joel Rorseth <joelrorseth@gmail.com>
2017-04-04 17:23:55 -0400

    Tidy up indentation

--- main.cxx
+++ main.cxx
@@ -19,105 +19,105 @@
 
 int main(int argc, char* argv[])
 {
-  using namespace std;
+    using namespace std;
 
-  // 1. Create an object of type cs340::program_options. Call it args.
-  // Pass into the constructor argc and argv as parameters.
-  //
-  // TODO: Read and examine the program_options.hxx header file.
-  // TODO: Read the documentation about Boost.ProgramOptions here:
-  //       http://www.boost.org/doc/libs/1_63_0/doc/html/program_options.html
-  // TODO: The program options code has been provided to you. Notice
-  //       how it simplifies processing command line arguments.
+    // 1. Create an object of type cs340::program_options. Call it args.
+    // Pass into the constructor argc and argv as parameters.
+    //
+    // TODO: Read and examine the program_options.hxx header file.
+    // TODO: Read the documentation about Boost.ProgramOptions here:
+    //       http://www.boost.org/doc/libs/1_63_0/doc/html/program_options.html
+    // TODO: The program options code has been provided to you. Notice
+    //       how it simplifies processing command line arguments.
 
     cs340::program_options args{argc, argv};
 
-  // 2. Set up the random number generator. Create an object of type
-  // std::seed_seq. Pass in as parameters an iterator pointing to the
-  // first element of args.seeds, and an iterator pointing to one
-  // past the end of the elements of args.seeds.
-  //
-  // Once done, create an object of type cs340::random_generator. Call it
-  // engine. Pass in your seed sequence as the only parameter to engine's
-  // unary constructor.
-  //
-  // TODO: Read about std::seed_seq in the course textbooks.
-  // TODO: Read about random numbers (e.g., <random>) in the course textbooks.
+    // 2. Set up the random number generator. Create an object of type
+    // std::seed_seq. Pass in as parameters an iterator pointing to the
+    // first element of args.seeds, and an iterator pointing to one
+    // past the end of the elements of args.seeds.
+    //
+    // Once done, create an object of type cs340::random_generator. Call it
+    // engine. Pass in your seed sequence as the only parameter to engine's
+    // unary constructor.
+    //
+    // TODO: Read about std::seed_seq in the course textbooks.
+    // TODO: Read about random numbers (e.g., <random>) in the course textbooks.
 
     // Create random generator
     std::seed_seq seq(begin(args.seeds), end(args.seeds));
     cs340::random_generator engine{seq};
 
-  // 3. Create an object of type cs340::simulation_parameters. Call it
-  // params. Pass in as arguments (in this exact order) to the constructor:
-  //
-  // 1. The number of generations
-  // 2. The min pool size
-  // 3. The number of threads.
-  //
-  // See the program_options.hxx for the correct member variables of your
-  // args object.
- 
-  cs340::simulation_parameters params{args.generations, args.min_pool_size, args.threads};
+    // 3. Create an object of type cs340::simulation_parameters. Call it
+    // params. Pass in as arguments (in this exact order) to the constructor:
+    //
+    // 1. The number of generations
+    // 2. The min pool size
+    // 3. The number of threads.
+    //
+    // See the program_options.hxx for the correct member variables of your
+    // args object.
 
-  // 4. Create a matrix object by calling the function
-  // cs340::create_random_matrix. Pass in the correct parameters
-  // (see types.hxx and types.cxx) for the interface. Use the value
-  // 30 for the time_max parameter.
+    cs340::simulation_parameters params{args.generations, args.min_pool_size, args.threads};
 
-  // Create random matrix using program options
-  auto random_matrix = 
-  	cs340::create_random_matrix(args.tasks, args.machines, 30, engine);
-  
-  cout << "Pool\tResult\tTime (s)\n";
-  for ( ;
-    params.pool_size <= args.max_pool_size;
-    params.pool_size += args.pool_size_step
-  ) 
-  {
-    // TODO: Read about <chrono> and related time facilities in your
-    //       course textbooks.
-   
-    // 5. Do the following, in order:
-    //
-    // a. Get the current CPU time. use std::chrono::high_resolution_clock.
-    //
-    // b. Call the function cs340::run_simulation. Check the header files
-    // to make sure you are passing the correct arguments! Store the
-    // return value of the function in a variable called result.
-    //
-    // c. Get the current CPU time the same way you did in step 5a.
-    //
-    // NOTE: Later you will be subtracting the values obtained in steps
-    //       5a and 5c to determine the total elapsed time of the
-    //       simulation.
+    // 4. Create a matrix object by calling the function
+    // cs340::create_random_matrix. Pass in the correct parameters
+    // (see types.hxx and types.cxx) for the interface. Use the value
+    // 30 for the time_max parameter.
 
-      auto cpu_time_before = std::chrono::high_resolution_clock::now();
+    // Create random matrix using program options
+    auto random_matrix = 
+        cs340::create_random_matrix(args.tasks, args.machines, 30, engine);
 
-      // Get resulting schedule object from the run_simulation() function
-      auto result = cs340::run_simulation(random_matrix, params, engine);
-      
-      auto cpu_time_after = std::chrono::high_resolution_clock::now();
+    cout << "Pool\tResult\tTime (s)\n";
+    for ( ;
+            params.pool_size <= args.max_pool_size;
+            params.pool_size += args.pool_size_step
+        ) 
+    {
+        // TODO: Read about <chrono> and related time facilities in your
+        //       course textbooks.
 
-    // 6. Output to standard out the following:
-    //
-    // i. params.pool_size
-    // 11. the score of the variable result
-    // iii. the number of seconds the simulation took to run. 
-    //
-    // For (iii), first compute a variable of type chrono::duration<double> 
-    // representing the difference between the stop time and the start time. 
-    // Then call .count() on it to get the number of seconds it represents.
-    //
-    // Each field output should be separated by a tab character.
-    // The entire output should be flushed via std::endl.
-    
-      std::chrono::duration<double> dif = cpu_time_after - cpu_time_before;
+        // 5. Do the following, in order:
+        //
+        // a. Get the current CPU time. use std::chrono::high_resolution_clock.
+        //
+        // b. Call the function cs340::run_simulation. Check the header files
+        // to make sure you are passing the correct arguments! Store the
+        // return value of the function in a variable called result.
+        //
+        // c. Get the current CPU time the same way you did in step 5a.
+        //
+        // NOTE: Later you will be subtracting the values obtained in steps
+        //       5a and 5c to determine the total elapsed time of the
+        //       simulation.
 
-      // Display information about the resulting schedule object, time stats
-      std::cout << params.pool_size << '\t' << result.score(random_matrix) 
-      	<< '\t' << dif.count() << std::endl;
-  }
+        auto cpu_time_before = std::chrono::high_resolution_clock::now();
+
+        // Get resulting schedule object from the run_simulation() function
+        auto result = cs340::run_simulation(random_matrix, params, engine);
+
+        auto cpu_time_after = std::chrono::high_resolution_clock::now();
+
+        // 6. Output to standard out the following:
+        //
+        // i. params.pool_size
+        // 11. the score of the variable result
+        // iii. the number of seconds the simulation took to run. 
+        //
+        // For (iii), first compute a variable of type chrono::duration<double> 
+        // representing the difference between the stop time and the start time. 
+        // Then call .count() on it to get the number of seconds it represents.
+        //
+        // Each field output should be separated by a tab character.
+        // The entire output should be flushed via std::endl.
+
+        std::chrono::duration<double> dif = cpu_time_after - cpu_time_before;
+
+        // Display information about the resulting schedule object, time stats
+        std::cout << params.pool_size << '\t' << result.score(random_matrix) 
+            << '\t' << dif.count() << std::endl;
+    }
 }
 
 //------------------------------------------------------------------------------
--- types.cxx
+++ types.cxx
@@ -19,170 +19,170 @@ using namespace std;
 
 namespace cs340 
 {
-  ostream& operator << (ostream& os, schedule const& c)
-  {
-    os << "{ ";
-    for (size_t i{}; i < c.tasks(); ++i)
-      os << c.task_assignment(i) << ' ';
-    os << '}';
-    return os;
-  }
-
-  std::ostream& operator << (std::ostream& os, runtime_matrix const& matrix)
-  {
-    for (size_t i{}; i < matrix.tasks(); ++i) 
+    ostream& operator << (ostream& os, schedule const& c)
     {
-      for (size_t j{}; j < matrix.machines(); ++j)
-        os << matrix(i, j) << ' ';
-      os << '\n';
+        os << "{ ";
+        for (size_t i{}; i < c.tasks(); ++i)
+            os << c.task_assignment(i) << ' ';
+        os << '}';
+        return os;
     }
-    return os;
-  }
 
-  // Generate a random matrix with the given dimensions.
-  runtime_matrix create_random_matrix(size_t const t, size_t const m,
-    size_t const time_max, random_generator& gen)
-  {
-    // 1. Create an object of type runtime_matrix with t tasks and m machines.
-    // Here, elements_ is initialized with size, so is t, m, tasks_, machines_
-	cs340::runtime_matrix matrix{t, m};	
-
-
-    // 2. Create a uniform_int_distribution that samples size_t's
-    // from the range [0, time_max].
-
-	uniform_int_distribution<std::size_t> dist{0,time_max};
-
-    // 3. Write a nested for loop to fill the matrix with random
-    // values. Use the distribution object (step 2) and the random 
-    // generator (i.e., gen) passed in as an argument to generate
-    // suitable random values.
-    //
-    // HINT: The matrix must be filled in for all (i,j) values.
-    //       Use two for loops (one nested inside the other) to
-    //       populate these values.
-
-    // Properly access elements_ property of matrix, insert random values
-    for (std::size_t i = 0; i < t; ++i) {
-        for (std::size_t j = 0; j < m; ++j) {
-            matrix(i, j) = dist(gen);
+    std::ostream& operator << (std::ostream& os, runtime_matrix const& matrix)
+    {
+        for (size_t i{}; i < matrix.tasks(); ++i) 
+        {
+            for (size_t j{}; j < matrix.machines(); ++j)
+                os << matrix(i, j) << ' ';
+            os << '\n';
         }
+        return os;
     }
 
-    // 4. Return the matrix.
-    return matrix;
-  }
-
-  // We compute the score of each schedule via it's makespan.  The
-  // makespan of a schedule is simply the total time from start to
-  // finish. In our case, that means the makespan is the maximum time
-  // any given machine will take to finish all of the tasks assigned
-  // to it.
-  //
-  // Since a lower makespan is better, but a higher score means a
-  // solution is more fit, we invert the makespan and pass the result
-  // through a smoothening function to get a decent score.
-  double schedule::score(runtime_matrix const& matrix) const
-  {
-    // 1. Check if data_ is empty. If so, return zero.
-    //
-    // REQUIREMENT: Do NOT use .size() to determine if data_ is empty!
-
-    if ( tasks() == 0 )
-    	return 0;
-
-    // 2. Check if we already have a cached score. If so,
-    // return the score we've cached. (See the class declaration
-    // in types.hxx.)
-
-    if (has_cache_)
-        return cached_score_;
-
-    // 3. We need to compute the score. First, create an object
-    // of type std::multimap<std::size_t, std::size_t>, which
-    // will map machines to the tasks that will run on it (as dictated
-    // by the schedule). Call this variable machine_schedule.
-
-    std::multimap<std::size_t, std::size_t> machine_schedule;
-
-    // Populate the multimap with a mutable lambda and for_each.
-    for_each(
-      begin(data_),
-      end(data_),
-      [&machine_schedule, i = 0](auto const& m) mutable 
-      {
-        machine_schedule.emplace(m, i);
-        ++i;
-      }
-    );
-
-    // 4. Next we compute the total runtime for each machine, and
-    // store the maximum of them. First create a variable called
-    // total_runtime. Assign it to the initial value of
-    // std::numeric_limits<std::size_t>::min(), which is the smallest
-    // value that can be stored in a size_t.
-
-    auto total_runtime = std::numeric_limits<std::size_t>::min();
-
-    for (size_t m = 0; m < matrix.machines(); ++m) 
+    // Generate a random matrix with the given dimensions.
+    runtime_matrix create_random_matrix(size_t const t, size_t const m,
+            size_t const time_max, random_generator& gen)
     {
-        // 5. Find each task assigned to the machine by using
-        // machine_schedule's .equal_range() member function. Pass
-        // in m as the parameter.
+        // 1. Create an object of type runtime_matrix with t tasks and m machines.
+        // Here, elements_ is initialized with size, so is t, m, tasks_, machines_
+        cs340::runtime_matrix matrix{t, m};	
 
-        // Find all pairs with machine m
-        // Returns a std::pair<iterator, iterator> with range of found elements
-        auto range = machine_schedule.equal_range(m);
 
-        // 6. Using std::accumulate along with the range returned
-        // from the call to equal_range, compute the total run time
-        // for this machine. Use a lambda, capturing the matrix by
-        // reference, to compute the sum.
+        // 2. Create a uniform_int_distribution that samples size_t's
+        // from the range [0, time_max].
+
+        uniform_int_distribution<std::size_t> dist{0,time_max};
+
+        // 3. Write a nested for loop to fill the matrix with random
+        // values. Use the distribution object (step 2) and the random 
+        // generator (i.e., gen) passed in as an argument to generate
+        // suitable random values.
         //
-        // HINTS:
-        //   *) The type of value you are accumulating is size_t.
-        //   *) Notice that the lambda's arguments don't modify 
-        //      anything so they should be const&.
-        //   *) You want to add the accumulated value to the next
-        //      value. The value being accumulated is accessible 
-        //      via matrix's function call operator which has 
-        //      two arguments, m(i,j). Refer to the definition
-        //      of RT[i,j] in types.hxx to pass the correct values
-        //      to m(i,j).
+        // HINT: The matrix must be filled in for all (i,j) values.
+        //       Use two for loops (one nested inside the other) to
+        //       populate these values.
 
-        // Compute total runtime for current machine
-        std::size_t total_runtime_machine = std::accumulate(range.first, range.second, 0,
-            [&matrix](auto const & a, auto const & b) -> std::size_t {
+        // Properly access elements_ property of matrix, insert random values
+        for (std::size_t i = 0; i < t; ++i) {
+            for (std::size_t j = 0; j < m; ++j) {
+                matrix(i, j) = dist(gen);
+            }
+        }
 
-                // Add accumulated (a) to next (matrix)
-                return a + matrix(b.first, b.second);
-        });
-
-        // 7. Using std::max, update the total_runtime variable declared
-        // outside of the loop.
-        
-        // Test if the computed runtime from this iteration is the new max
-        total_runtime = std::max(total_runtime, total_runtime_machine);
+        // 4. Return the matrix.
+        return matrix;
     }
 
-
-
-    // 8. Now compute the cached score by using the following
-    // formula:
+    // We compute the score of each schedule via it's makespan.  The
+    // makespan of a schedule is simply the total time from start to
+    // finish. In our case, that means the makespan is the maximum time
+    // any given machine will take to finish all of the tasks assigned
+    // to it.
     //
-    //     cached score = 1 / (total runtime + 1) * 1000
-    //
-    // Also, set the has_cache_ flag to true, so we avoid having
-    // to recompute this value the next time we need it.
+    // Since a lower makespan is better, but a higher score means a
+    // solution is more fit, we invert the makespan and pass the result
+    // through a smoothening function to get a decent score.
+    double schedule::score(runtime_matrix const& matrix) const
+    {
+        // 1. Check if data_ is empty. If so, return zero.
+        //
+        // REQUIREMENT: Do NOT use .size() to determine if data_ is empty!
 
-    // Calculate cached score
-    // NOTE: Must use float (1.0) to force implicit conversion
-    cached_score_ = 1.0 / (total_runtime + 1) * 1000;
-	has_cache_ = true;
+        if ( tasks() == 0 )
+            return 0;
 
-    // 9. Finally, return the cached score you computed.
-    return cached_score_;
-  }
+        // 2. Check if we already have a cached score. If so,
+        // return the score we've cached. (See the class declaration
+        // in types.hxx.)
+
+        if (has_cache_)
+            return cached_score_;
+
+        // 3. We need to compute the score. First, create an object
+        // of type std::multimap<std::size_t, std::size_t>, which
+        // will map machines to the tasks that will run on it (as dictated
+        // by the schedule). Call this variable machine_schedule.
+
+        std::multimap<std::size_t, std::size_t> machine_schedule;
+
+        // Populate the multimap with a mutable lambda and for_each.
+        for_each(
+                begin(data_),
+                end(data_),
+                [&machine_schedule, i = 0](auto const& m) mutable 
+                {
+                machine_schedule.emplace(m, i);
+                ++i;
+                }
+                );
+
+        // 4. Next we compute the total runtime for each machine, and
+        // store the maximum of them. First create a variable called
+        // total_runtime. Assign it to the initial value of
+        // std::numeric_limits<std::size_t>::min(), which is the smallest
+        // value that can be stored in a size_t.
+
+        auto total_runtime = std::numeric_limits<std::size_t>::min();
+
+        for (size_t m = 0; m < matrix.machines(); ++m) 
+        {
+            // 5. Find each task assigned to the machine by using
+            // machine_schedule's .equal_range() member function. Pass
+            // in m as the parameter.
+
+            // Find all pairs with machine m
+            // Returns a std::pair<iterator, iterator> with range of found elements
+            auto range = machine_schedule.equal_range(m);
+
+            // 6. Using std::accumulate along with the range returned
+            // from the call to equal_range, compute the total run time
+            // for this machine. Use a lambda, capturing the matrix by
+            // reference, to compute the sum.
+            //
+            // HINTS:
+            //   *) The type of value you are accumulating is size_t.
+            //   *) Notice that the lambda's arguments don't modify 
+            //      anything so they should be const&.
+            //   *) You want to add the accumulated value to the next
+            //      value. The value being accumulated is accessible 
+            //      via matrix's function call operator which has 
+            //      two arguments, m(i,j). Refer to the definition
+            //      of RT[i,j] in types.hxx to pass the correct values
+            //      to m(i,j).
+
+            // Compute total runtime for current machine
+            std::size_t total_runtime_machine = std::accumulate(range.first, range.second, 0,
+                    [&matrix](auto const & a, auto const & b) -> std::size_t {
+
+                    // Add accumulated (a) to next (matrix)
+                    return a + matrix(b.first, b.second);
+                    });
+
+            // 7. Using std::max, update the total_runtime variable declared
+            // outside of the loop.
+
+            // Test if the computed runtime from this iteration is the new max
+            total_runtime = std::max(total_runtime, total_runtime_machine);
+        }
+
+
+
+        // 8. Now compute the cached score by using the following
+        // formula:
+        //
+        //     cached score = 1 / (total runtime + 1) * 1000
+        //
+        // Also, set the has_cache_ flag to true, so we avoid having
+        // to recompute this value the next time we need it.
+
+        // Calculate cached score
+        // NOTE: Must use float (1.0) to force implicit conversion
+        cached_score_ = 1.0 / (total_runtime + 1) * 1000;
+        has_cache_ = true;
+
+        // 9. Finally, return the cached score you computed.
+        return cached_score_;
+    }
 }
 
 //------------------------------------------------------------------------------
